527087e22920f92cfbece1b27e354f9a4e82c7bd
nigamharsh <himanshu.nigam_cs18@gla.ac.in>
2021-05-23 12:22:14 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myshoppers/AdminMaintainProductsActivity.java
@@ -0,0 +1,14 @@
+package com.example.myshoppers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminMaintainProductsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_maintain_products);
+    }
+}
---
+++ app/src/main/java/com/example/myshoppers/prevalent/Prevalent.java
@@ -0,0 +1,13 @@
+package com.example.myshoppers.prevalent;
+
+import com.example.myshoppers.model.Users;
+
+public class Prevalent
+{
+    public static Users currentOnlineUser;
+
+
+    public static String UserPhoneKey="UserPhone";
+    public static String UserPasswordKey="UserPassword";
+
+}
